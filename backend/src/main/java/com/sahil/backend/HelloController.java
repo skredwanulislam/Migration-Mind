727758271faefe68f95fa54d8866a20afb978ec4
Sahil al farib <117147361+sahilaf@users.noreply.github.com>
2025-11-29 23:52:17 +0600

    Initial commit

---
+++ backend/src/main/java/com/sahil/backend/HelloController.java
@@ -0,0 +1,14 @@
+package com.sahil.backend;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+    
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="bg-amber-300 text-6xl">Hellow world</h1>
+    </div>
+  )
+}
+
+export default App
